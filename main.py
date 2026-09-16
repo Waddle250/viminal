67bbf39f645ee37e7e5d3baba63fa478edf04b3e
Waddle250 <loganaquino11@hotmail.com>
2026-09-16 10:16:54 -0400

    Fixed a few errors and added maxFrames parameter to help with playback

--- main.py
+++ main.py
@@ -6,19 +6,20 @@ timestamp = 0
 def playVideo(stdscr, video, width, height):
   global timestamp
   capturedVideo = cv2.VideoCapture(video)
-  success, frame = capturedVideo.read
+  success, frame = capturedVideo.read()
+  maxFrames = capturedVideo.get(cv2.CAP_PROP_FRAME_COUNT)
   stdscr.clear()
   
   x = 0
   y = 0
 
   #loop that renders each frame
-  
-  for y in range(height):
-    try:
-      for x in range(width):
-        stdscr.addstr(y, x, '#')
-  timestamp += 1
+  for f in range(maxFrames):
+    for y in range(height):
+      try:
+        for x in range(width):
+          stdscr.addstr(y, x, '#')
+    timestamp += 1
 
   stdscr.clear()
   print(f"Video finished successfully at {str(timestamp)}")
